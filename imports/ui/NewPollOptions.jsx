6f4a4e244c79c78ca572723799d46ad197eb5ef5
GilbertMizrahi <gilbertmizrahi@gmail.com>
2016-09-10 18:30:05 -0400

    In this branmch I added the functionality to add and to remove options from a poll

---
+++ imports/ui/NewPollOptions.jsx
@@ -0,0 +1,39 @@
+import React, { Component, PropTypes } from 'react';
+
+import OptionsData from '/imports/api/newPollOptions.js';
+
+export default class NewPollOption extends Component {
+
+  onOptionChange(event) {
+    let id = this.props.option._id;
+    console.log("ref "+this.props.option.index+" id "+id);
+    let option = event.target.value;
+    this.value = option;
+    OptionsData.update(id, {$set: {option: option}}, function(error, result) {
+      console.log(OptionsData.find({}, {sort: {index: 1}}).fetch())
+      if (error) {
+        // display the error to the user
+        alert.error(error.reason);
+      }
+    });
+  }
+
+  deleteOption(e){
+    e.preventDefault();
+    const id = this.props.option._id;
+    OptionsData.remove(id);
+  }
+
+  render() {
+
+    return (
+      <div className="input-option">
+        <input type="text" className="optionInput" defaultValue={this.props.option.option}
+          onChange={this.onOptionChange.bind(this)} placeholder="Enter option" />
+        <a href="#" className="remove-option " onClick={this.deleteOption.bind(this)}>
+          <i className="fa fa-minus-square"></i>
+        </a>
+      </div>
+    );
+  }
+}
