efebbd450bac9befa6805f4a1574d5b245b457fb
GilbertMizrahi <gilbertmizrahi@gmail.com>
2016-09-12 12:51:59 -0400

    code to make navbar responsive part 2

---
+++ imports/ui/NewPoll.jsx
@@ -0,0 +1,11 @@
+import React, {Component, PropTypes} from 'react';
+
+export default class NewPOll extends Component {
+  render() {
+    return (
+      <div className="new-poll">
+        <h2>Some stuff will be here</h2>
+      </div>
+    )
+  }
+}
