4172a27558c29074b8b0b93d61d6311c452b40d4
GilbertMizrahi <gilbertmizrahi@gmail.com>
2016-09-12 18:56:06 -0400

    add event to delete a poll and alert to user before proceding

---
+++ imports/ui/Poll.jsx
@@ -0,0 +1,56 @@
+import React, { Component, PropTypes } from 'react';
+import moment from 'moment';
+
+import Option from "./Option.jsx";
+
+export default class Poll extends Component {
+  deletePoll() {
+    const currentPollId = this.props.poll._id;
+    swal({
+      title: "Are you sure you want to delete this poll?",
+      text: "You will not be able to recover this if you do so!",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "Yes, delete it!",
+      closeOnConfirm: true,
+      html: false
+    },
+    function(isConfirm){
+      if(isConfirm){
+        Meteor.call("deletePoll", currentPollId);
+      }
+    });
+  }
+  formatDate(date) {
+    return moment(date).fromNow();
+  }
+  render() {
+    return (
+      <div className="poll">
+        <div className="question">
+          <h3 className="">{this.props.poll.question}</h3>
+            <span className="pull-right icons">
+              <a href="#"><i className="fa fa-eye view-poll"></i></a>
+              <a href="#"><i className="fa fa-pencil edit-poll"></i></a>
+              <a href="#" onClick={this.deletePoll.bind(this)}><i className="fa fa-trash-o delete-poll" ></i></a>
+            </span>
+        </div>
+        <div className="panel-body">
+          <Option options={this.props.poll.options}
+            key={this.props.poll.options.index}/>
+          <div className="poll-footer">
+            Created by {this.props.poll.author} <span
+              className="badge badge-info pull-right">{this.props.poll.totalVotes}</span>
+            <br/>
+            {this.formatDate(this.props.poll.createdAt)}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Poll.propTypes = {
+  poll: PropTypes.object.isRequired,
+};
