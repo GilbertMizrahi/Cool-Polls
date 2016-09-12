f6ab32b91d3a705eda40d2775c4e60dcaae8f08e
GilbertMizrahi <gilbertmizrahi@gmail.com>
2016-09-12 14:06:35 -0400

    creating a sticky footer using flexbox

---
+++ client/route.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import {mount} from 'react-mounter';
+
+import {MainLayout} from '../imports/ui/layouts/mainLayout.jsx';
+import App from '../imports/ui/App.jsx';
+import Home from '../imports/ui/Home.jsx';
+import NewPoll from '../imports/ui/NewPoll.jsx';
+import Header from '../imports/ui/partials/header.jsx';
+import Footer from '../imports/ui/partials/footer.jsx';
+import NewUser from '../imports/ui/partials/register.jsx';
+import Login from '../imports/ui/partials/login.jsx';
+import ChangePassword from '../imports/ui/partials/changePassword.jsx';
+import ResetPassword from '../imports/ui/partials/resetPassword.jsx';
+
+FlowRouter.route('/', {
+  name: 'home',
+  action() {
+    mount(MainLayout, {
+      header: (<Header />),
+      content: (<Home />),
+      footer: (<Footer />)
+    })
+  }
+});
+
+FlowRouter.route('/polls', {
+  name: 'polls',
+  action() {
+    if(Meteor.userId()) {
+      mount(MainLayout, {
+        header: (<Header />),
+        content: (<App />),
+        footer: (<Footer />)
+      })
+    } else FlowRouter.go('home');
+  }
+});
+
+FlowRouter.route('/newPoll', {
+  name: 'newPoll',
+  action() {
+    if(Meteor.userId()) {
+      mount(MainLayout, {
+        header: (<Header />),
+        content: (<NewPoll />),
+        footer: (<Footer />)
+      })
+    } else FlowRouter.go('home');
+  }
+});
+
+FlowRouter.route('/login', {
+  name: 'login',
+  action(params) {
+    mount(MainLayout, {
+      header: (<Header />),
+      content: (<Login />)
+    })
+  }
+});
+
+FlowRouter.route('/register', {
+  name: 'register',
+  action() {
+    mount(MainLayout, {
+      header: (<Header />),
+      content: (<NewUser />)
+    })
+  }
+});
+
+FlowRouter.route('/change-password', {
+  name: 'change-password',
+  action() {
+    mount(MainLayout, {
+      header: (<Header />),
+      content: (<ChangePassword />)
+    })
+  }
+});
+
+FlowRouter.route('/reset-password', {
+  name: 'reset-password',
+  action() {
+    mount(MainLayout, {
+      header: (<Header />),
+      content: (<ResetPassword />)
+    })
+  }
+});
